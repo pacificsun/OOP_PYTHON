14636e49aa497487ec30728aa0fb1b13bcafd208
Suraj Thapa <stsurajst@gmail.com>
2023-03-04 09:43:08 -0600

    Constructor fix with self and attribute also add more attribute to object shonw in line 22

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    # Construtor with quantity default to zero
+    def __init__(self, name, price, quantity = 0):
+
         self.name = name
         self.price = price
         self.quantity = quantity
-    def calculate_total_price(self, x, y):
 
-
-        return x*y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 item1 = Item("Laptop", 50, 3)
 item2 = Item("Mobile", 10000, 20)
 
 print(item2.name)
+
+print(item1.calculate_total_price())
+
+item1.has_numpad = False
+
+print(item1.has_numpad)
