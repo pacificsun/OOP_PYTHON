06e18a4cc678d2d0ec6e5c2949735d02f063ed1f
Suraj Thapa <stsurajst@gmail.com>
2023-03-04 09:23:02 -0600

    Constructor added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
     def calculate_total_price(self, x, y):
+
 
         return x*y
 
 
-item1 = Item()
-item1.price = 10
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("Laptop", 50, 3)
+item2 = Item("Mobile", 10000, 20)
+
+print(item2.name)
